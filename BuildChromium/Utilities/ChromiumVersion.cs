3fbe9115cf3c1bc7aea25f43548bc3b3e9fe11bd
William Kent <wjk011@gmail.com>
2017-02-24 22:04:34 -0500

    Add project files.

---
+++ BuildChromium/Utilities/ChromiumVersion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
+
+namespace BuildChromium.Utilities
+{
+    public struct SourceVersion
+    {
+        public static SourceVersion ParseXml(FileInfo file)
+        {
+            using (var stream = file.OpenRead())
+            {
+                XDocument doc = XDocument.Load(stream);
+                XElement root = doc.Root.Element(nameof(SourceVersion));
+
+                SourceVersion vers = new SourceVersion();
+                vers.ChromiumVersion = root.Element(nameof(vers.ChromiumVersion)).Value;
+                vers.ReleaseRevision = root.Element(nameof(vers.ReleaseRevision)).Value;
+                return vers;
+            }
+        }
+
+        public string ChromiumVersion { get; set; }
+        public string ReleaseRevision { get; set; }
+    }
+}
---
+++ BuildChromium/Utilities/ExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace BuildChromium.Utilities
+{
+    public static class ExtensionMethods
+    {
+        public static XElement Element(this XElement parent, string localName) => parent.Element(XName.Get(localName, ""));
+        public static XAttribute Attribute(this XElement element, string localName) => element.Attribute(XName.Get(localName, ""));
+    }
+}
