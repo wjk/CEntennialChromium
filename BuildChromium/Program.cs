88f0d7e18b95cf1ac0052899c834e2ca9336f0c1
William Kent <wjk011@gmail.com>
2017-02-25 17:41:24 -0500

    Add logfile parameter to BuildChromium
    This change makes it possible to log build status to one or more
    files as well as to the console.

--- BuildChromium/Program.cs
+++ BuildChromium/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.CommandLine;
+using System.IO;
 using System.Linq;
 using BuildChromium.Logging;
 using BuildChromium.Utilities;
@@ -18,6 +19,7 @@ namespace BuildChromium
                 bool build64Bit = false;
                 string patchSetPath = null, sandboxDirectory = null;
                 IReadOnlyList<string> patchSetsToApply = Array.Empty<string>();
+                IReadOnlyList<string> logFiles = Array.Empty<string>();
 
                 // Note: System.CommandLine currently requires that I provide each parameter that is
                 // common to all subcommands repeatedly, once for each command. Yes, it's stupid.
@@ -26,19 +28,39 @@ namespace BuildChromium
                     syntax.ApplicationName = "BuildChromium";
 
                     syntax.DefineCommand("prepare");
+                    syntax.DefineOptionList("logfile", ref logFiles, "Write status messages to this file");
                     syntax.DefineParameter("sandbox-directory", ref sandboxDirectory, "The directory to clone Chromium into");
 
                     syntax.DefineCommand("patch");
+                    syntax.DefineOptionList("logfile", ref logFiles, "Write status messages to this file");
                     syntax.DefineOption("patchset-dir", ref patchSetPath, "Directory containing the patchset files");
                     syntax.DefineOptionList("p|apply-patchset", ref patchSetsToApply, "Apply these patchsets");
                     syntax.DefineParameter("sandbox-directory", ref sandboxDirectory, "The directory to clone Chromium into");
 
                     syntax.DefineCommand("build");
+                    syntax.DefineOptionList("logfile", ref logFiles, "Write status messages to this file");
                     syntax.DefineOption("patchset-dir", ref patchSetPath, "Directory containing the patchset files");
                     syntax.DefineOptionList("p|apply-patchset", ref patchSetsToApply, "Apply these patchsets");
                     syntax.DefineOption("x64", ref build64Bit, "Build a 64-bit binary");
                     syntax.DefineParameter("sandbox-directory", ref sandboxDirectory, "The directory to clone Chromium into");
                 });
+
+                foreach (string path in logFiles)
+                {
+                    if (!Directory.Exists(Path.GetDirectoryName(path)))
+                    {
+                        Log.Write(LogLevel.Warning, "Cannot log to file {0}: Path does not exist. Ignoring this file.", Path.GetFileName(path));
+                        continue;
+                    }
+
+                    if (Directory.Exists(path))
+                    {
+                        Log.Write(LogLevel.Warning, "Cannot log to file {0}: Is a directory. Ignoring this file.", path);
+                        continue;
+                    }
+
+                    Log.AddLogger(new FileLogger(new FileInfo(path)));
+                }
             }
             catch (Exception ex)
             {
