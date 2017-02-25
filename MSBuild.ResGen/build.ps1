f53a553ed18b388af701b63ae3ba264e2ecb60c1
William Kent <wjk011@gmail.com>
2017-02-25 14:14:36 -0500

    Fix MSBuild.ResGen build script

--- MSBuild.ResGen/build.ps1
+++ MSBuild.ResGen/build.ps1
@@ -2,5 +2,5 @@
 & dotnet build
 
 mkdir bin\BuildTasks\, bin\BuildTasks\net46, bin\BuildTasks\netcoreapp1.1 -ea 0 > $null
-Copy-Item bin\Debug\net46\MSBuild.ResGen.dll ..\..\bin\BuildTasks\net46
-Copy-Item bin\Debug\netcoreapp1.1\MSBuild.ResGen.dll ..\..\bin\BuildTasks\netcoreapp1.1
+Copy-Item bin\Debug\net46\MSBuild.ResGen.dll bin\BuildTasks\net46
+Copy-Item bin\Debug\netcoreapp1.1\MSBuild.ResGen.dll bin\BuildTasks\netcoreapp1.1
