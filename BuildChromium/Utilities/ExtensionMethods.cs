4bc44931f8a4ea76fc8d5e62e9247bec0a64b42a
William Kent <wjk011@gmail.com>
2017-02-25 21:38:34 -0500

    Add ExtensionMethods.GetSubdirectory()
    This method hlps avoid the fact that DirectoryInfo.CreateDirectory()
    throws an IOException if the directory given already exists.

--- BuildChromium/Utilities/ExtensionMethods.cs
+++ BuildChromium/Utilities/ExtensionMethods.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Xml.Linq;
 
@@ -9,5 +10,12 @@ namespace BuildChromium.Utilities
     {
         public static XElement Element(this XElement parent, string localName) => parent.Element(XName.Get(localName, ""));
         public static XAttribute Attribute(this XElement element, string localName) => element.Attribute(XName.Get(localName, ""));
+
+        public static DirectoryInfo GetSubdirectory(this DirectoryInfo parent, string path)
+        {
+            string subdirPath = Path.Combine(parent.FullName, path);
+            if (Directory.Exists(subdirPath)) return new DirectoryInfo(subdirPath);
+            else return parent.CreateSubdirectory(path);
+        }
     }
 }
