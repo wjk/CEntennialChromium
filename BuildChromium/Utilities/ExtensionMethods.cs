d25184003637c5f6a5a3509e7bbbac7b6d4b6b9e
William Kent <wjk011@gmail.com>
2017-02-25 21:50:59 -0500

    Add ExtensionMethods.CreateFile() method
    This method gets a FileInfo instance with a given name in a given
    directory, creating it if required.

--- BuildChromium/Utilities/ExtensionMethods.cs
+++ BuildChromium/Utilities/ExtensionMethods.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Xml.Linq;
 
@@ -10,6 +11,22 @@ namespace BuildChromium.Utilities
     {
         public static XElement Element(this XElement parent, string localName) => parent.Element(XName.Get(localName, ""));
         public static XAttribute Attribute(this XElement element, string localName) => element.Attribute(XName.Get(localName, ""));
+
+        public static FileInfo CreateFile(this DirectoryInfo parent, string fileName)
+        {
+            if (parent == null) throw new ArgumentNullException(nameof(parent));
+            if (fileName == null) throw new ArgumentNullException(nameof(fileName));
+            if (fileName.Contains(Path.DirectorySeparatorChar) || fileName.Contains(Path.AltDirectorySeparatorChar)) throw new ArgumentException("File name cannot contain directory separators", nameof(fileName));
+            if (fileName.Contains("*") || fileName.Contains("?")) throw new ArgumentException("File name cannot contain wildcard characters", nameof(fileName));
+
+            FileInfo[] existingFiles = parent.GetFiles(fileName);
+            if (existingFiles.Length == 1) return existingFiles[0];
+            else if (existingFiles.Length > 1) throw new InvalidOperationException($"File name '{fileName}' matched more than one file");
+
+            string fullPath = Path.Combine(parent.FullName, fileName);
+            File.WriteAllBytes(fullPath, Array.Empty<byte>());
+            return new FileInfo(fullPath);
+        }
 
         public static DirectoryInfo GetSubdirectory(this DirectoryInfo parent, string path)
         {
